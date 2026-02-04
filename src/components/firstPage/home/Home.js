2d616b35db7b0b70942fb693602de6fe6a4510a2
Noam597 <noam_harris@hotmail.com>
2026-02-04 18:08:00 +0200

    updated PDF resume

--- src/components/firstPage/home/Home.js
+++ src/components/firstPage/home/Home.js
@@ -2,7 +2,7 @@
 import styles from "./home.module.css";
 import {AiFillLinkedin,AiFillGithub} from 'react-icons/ai';
 import {FaFileDownload} from 'react-icons/fa';
-import CV from '../../../files/Fullstack_cv.pdf'
+import CV from '../../../files/Fullstack_cv1.pdf'
 const Home = () => {
   return (
     <section id="home" className={styles.home}>
